23bf947cc43136ca2006a42053975b3d2921387f
Victor Garcia <vichor@gmail.com>
2018-07-27 16:22:47 +0200

    Add Index Buffers rendering.

--- src/engineTester/MainGameLoop.java
+++ src/engineTester/MainGameLoop.java
@@ -19,17 +19,17 @@ public class MainGameLoop {
 		
 		// OpenGL expects vertices to be defined counter clockwise by default
 		float[] vertices = {
-				// left bottom triangle
-				-0.5f, 0.5f, 0f,
-				-0.5f, -0.5f, 0f,
-				0.5f, -0.5f, 0f,
-				// right top triangle
-				0.5f, -0.5f, 0f,
-				0.5f, 0.5f, 0f,
-				-0.5f, 0.5f, 0f
+				-0.5f, 0.5f, 0f,	// V0
+				-0.5f, -0.5f, 0f,	// V1
+				0.5f, -0.5f, 0f,	// V2
+				0.5f, 0.5f, 0f		// V3
+		};
+		int[] indices = {
+				0, 1, 3, // top left triangle
+				3, 1, 2  // bottom right triangle
 		};
 		
-		RawModel model = loader.loadToVAO(vertices);
+		RawModel model = loader.loadToVAO(vertices, indices);
 		
 		while(!Display.isCloseRequested() ) {
 			renderer.prepare();
