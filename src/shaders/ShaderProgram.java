6685af7e8c4359c60bf109b7f4bbb51f4b702e98
Victor Garcia <vichor@gmail.com>
2018-07-30 15:15:17 +0200

    Add uniform support and transformation matrix generation.

--- src/shaders/ShaderProgram.java
+++ src/shaders/ShaderProgram.java
@@ -3,9 +3,13 @@ package shaders;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
+import java.nio.FloatBuffer;
 
+import org.lwjgl.BufferUtils;
 import org.lwjgl.opengl.GL11;
 import org.lwjgl.opengl.GL20;
+import org.lwjgl.util.vector.Matrix4f;
+import org.lwjgl.util.vector.Vector3f;
 
 public abstract class ShaderProgram {
 
@@ -13,7 +17,9 @@ public abstract class ShaderProgram {
 	private int vertexShaderID;
 	private int fragmentShaderID;
 	
+	private static FloatBuffer matrixBuffer = BufferUtils.createFloatBuffer(16);
 	
+
 	public ShaderProgram(String vertexFile, String fragmentFile) {
 		vertexShaderID = loadShader(vertexFile, GL20.GL_VERTEX_SHADER);
 		fragmentShaderID = loadShader(fragmentFile, GL20.GL_FRAGMENT_SHADER);
@@ -25,10 +31,20 @@ public abstract class ShaderProgram {
 		GL20.glValidateProgram(programID);
 	}
 	
+	
+	protected abstract void getAllUniformLocations();
+	
+	
+	protected int getUniformLocation(String uniformName) {
+		return GL20.glGetUniformLocation(programID, uniformName);
+	}
+	
+	
 	public void start() {
 		GL20.glUseProgram(programID);
 	}
 	
+
 	public void stop() {
 		GL20.glUseProgram(0);
 	}
@@ -51,6 +67,32 @@ public abstract class ShaderProgram {
 		GL20.glBindAttribLocation(programID, attribute, variableName);
 	}
 	
+	
+	protected void loadFloat(int location, float value) {
+		GL20.glUniform1f(location, value);
+	}
+	
+
+	protected void loadVector(int location, Vector3f vector) {
+		GL20.glUniform3f(location, vector.x, vector.y, vector.z);
+	}
+
+	
+	protected void loadBoolean(int location, boolean value) {
+		float toLoad = 0;
+		if (value) {
+			toLoad = 1;
+		}
+		GL20.glUniform1f(location, toLoad);
+	}
+	
+
+	protected void loadMatrix(int location, Matrix4f matrix) {
+		matrix.store(matrixBuffer);
+		matrixBuffer.flip();
+		GL20.glUniformMatrix4(location, false, matrixBuffer);
+	}
+
 	
 	private static int loadShader(String file, int type) {
 		StringBuilder shaderSource = new StringBuilder();
--- src/shaders/vertexShader.glsl
+++ src/shaders/vertexShader.glsl
@@ -5,8 +5,10 @@ in vec2 textureCoords;
 
 out vec2 pass_textureCoords;
 
+uniform mat4 transformationMatrix;
+
 void main(void) {
 
-	gl_Position = vec4(position, 1.0);
+	gl_Position = transformationMatrix * vec4(position, 1.0);
 	pass_textureCoords = textureCoords;
 }
