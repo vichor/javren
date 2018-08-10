0fe8ebd3abdbe88f86847fd5dd7737b8c40cbf40
Victor Garcia <vichor@gmail.com>
2018-08-10 12:55:34 +0200

    Add player jump feature. Also added offset to rotation to calculate movement needed because model is not drawn in blender looking forward.

--- src/entities/Player.java
+++ src/entities/Player.java
@@ -10,9 +10,20 @@ public class Player extends Entity {
 	
 	private static final float RUN_SPEED = 20;
 	private static final float TURN_SPEED = 160;
+	private static final float GRAVITY = -50;
+	private static final float JUMP_POWER = 40;
+	
+	private static final float TERRAIN_HEIGHT = 0;
+	
+	// The model is drawn in blender looking 90 degrees rotated (its look at front
+	// position is not ok). Added 90 degrees of offset to correct it.
+	private static final float LOOK_AT_FRONT_OFFSET = 90;
 	
 	private float currentSpeed = 0;
 	private float currentTurnSpeed = 0;
+	private float upwardsSpeed = 0;
+
+	private boolean isInAir = false;
 
 	public Player(TexturedModel model, Vector3f position, float rotX, float rotY, float rotZ, float scale) {
 		super(model, position, rotX, rotY, rotZ, scale);
@@ -28,9 +39,24 @@ public class Player extends Entity {
 		// of the axis will depend on the direction the player is looking to
 		// (its rotation angle in Y axis). 
 		float distance = currentSpeed * DisplayManager.getFrameTimeSeconds();
-		float dx = (float) (distance * Math.sin(Math.toRadians(super.getRotY())));
-		float dz = (float) (distance * Math.cos(Math.toRadians(super.getRotY())));
+		float dx = (float) (distance * Math.sin(Math.toRadians(super.getRotY()+LOOK_AT_FRONT_OFFSET)));
+		float dz = (float) (distance * Math.cos(Math.toRadians(super.getRotY()+LOOK_AT_FRONT_OFFSET)));
 		super.increasePosition(dx, 0, dz);
+		upwardsSpeed += GRAVITY * DisplayManager.getFrameTimeSeconds();
+		super.increasePosition(0, upwardsSpeed*DisplayManager.getFrameTimeSeconds(), 0);
+		if (super.getPosition().y < TERRAIN_HEIGHT) {
+			upwardsSpeed = 0;
+			isInAir = false;
+			super.getPosition().y = TERRAIN_HEIGHT;
+		}
+	}
+	
+	
+	private void jump() {
+		if (!isInAir) {
+			upwardsSpeed = JUMP_POWER;
+			isInAir = true;
+		}
 	}
 	
 	
@@ -49,6 +75,10 @@ public class Player extends Entity {
 			currentTurnSpeed = -TURN_SPEED;
 		} else {
 			currentTurnSpeed = 0;
+		}
+		
+		if (Keyboard.isKeyDown(Keyboard.KEY_SPACE)) {
+			jump();
 		}
 	}
 	
