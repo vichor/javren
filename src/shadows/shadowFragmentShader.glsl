8df88436f0137c9e94f36ad02cff9f3a4ae19917
Victor Garcia <vichor@gmail.com>
2018-11-13 08:42:50 +0100

    Deal with texture transparency in shadows to avoid bulky shadows and have greater detail.

--- src/shadows/shadowFragmentShader.glsl
+++ src/shadows/shadowFragmentShader.glsl
@@ -1,10 +1,19 @@
 #version 330
 
+in vec2 textureCoords;
+
 out vec4 out_colour;
 
-uniform sampler2D modelTexture;//will use this next week
+
+uniform sampler2D modelTexture; // transparency in textures.
 
 void main(void){
+
+	// deal with transparent textures: discard fragment if the alpha channel is below 0.5
+	float alpha = texture(modelTexture, textureCoords).a;
+	if (alpha < 0.5){
+		discard;
+	}
 
 	out_colour = vec4(1.0);
 	
--- src/shadows/shadowVertexShader.glsl
+++ src/shadows/shadowVertexShader.glsl
@@ -1,6 +1,9 @@
 #version 150
 
 in vec3 in_position;
+in vec2 in_textureCoords;
+
+out vec2 textureCoords;
 
 uniform mat4 mvpMatrix;
 
@@ -8,5 +11,7 @@ void main(void){
 
 	// get position in shadow map space
 	gl_Position = mvpMatrix * vec4(in_position, 1.0);
+	
+	textureCoords = in_textureCoords;
 
 }
